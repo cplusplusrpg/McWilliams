ee09a2dd0687c822298c650d0982269983560f4d
Andrew Namgoong <AGoon105@users.noreply.github.com>
2014-12-11 11:30:03 -0800

    Updated, included a display?

--- store_inventory.cpp
+++ store_inventory.cpp
@@ -1,18 +1,39 @@
-// Quick question: Does the store have to be THIS LONG?! T-T
+// WHY IS THIS SO LONG T_T
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include "Monster.h"
+#include "Player.h"
+#include "function.h"
+#include "Item.h"
 
 using namespace std;
 
 void store(vector<string>& inventory, vector<string>& potions);
+void show_inventory(vector<string> inventory, vector<string> potions);
+void show_inventory(vector<string> inventory, vector<string> potions)
+{
+    cout << "This is your current inventory." << endl;
+    cout << "***********" << endl;
+    cout << "*Inventory*" << endl;
+    cout << "***********" << endl;
+    for (unsigned int i = 0; i < potions.size(); i++)
+    {
+        cout << potions[i] << endl;
+    }
+    for (unsigned int i = 0; i < inventory.size(); i++)
+    {
+        cout << inventory[i] << endl;
+    }
+}
 
 int main()
 {
-    vector<string> a;
+    vector<string> inventory;
     vector<string> potions;
-    store(a, potions);
+    store(inventory, potions);
+    show_inventory(inventory, potions);
     return 0;
 }
 
@@ -118,7 +139,7 @@ void store(vector<string>& inventory, vector<string>& potions)
                 cout << "2) Claymore: Damage(10), Durability(300), Cost(150 gold)" << endl;
                 cout << "3) Gladius: Damage(20), Durability(400), Cost(500 gold)" << endl;
                 cout << "4) Soul Eater: Damage(100), Durability(1000), Cost(1000 gold)" << endl;
-                cout << "5) Dance of Flames: Damage(200), Durability(2000), Cost(10000 gold)" << endl;
+                cout << "5) Flame Dance: Damage(200), Durability(2000), Cost(10000 gold)" << endl;
                 cout << "6) Heartless Crusader: Damage(400), Durability(3500), Cost(100000 gold)" << endl;
                 cout << "7) Eternal Harmony: Damage(600), Durability(4000), Cost(500000 gold)" << endl;
                 cout << "8) Elucidator: Damage(1000), Durability(5000), Cost(1000000 gold)" << endl;
@@ -173,8 +194,8 @@ void store(vector<string>& inventory, vector<string>& potions)
                 if (option2 == 5)
                 {
                     // Say something here about not having enough gold
-                    cout << "Successfully purchased 1 Dance of Flames, 10000 gold will now be taken out of your account." << endl;
-                    inventory.push_back("Dance of Flames");
+                    cout << "Successfully purchased 1 Flame Dance, 10000 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Flame Dance");
                     // Deduct gold from the acoount
                     break;
                 }
@@ -203,9 +224,80 @@ void store(vector<string>& inventory, vector<string>& potions)
                     break;
                 }
             case 3:
-                
+                int a;
+                cout << "Choose some armor!" << endl;
+                cout << "1) Beginner Armor:     Defense(2)      Durability(100)     Cost(50 gold)" << endl;
+                cout << "2) Ebon Armor:         Defense(10)     Durability(300)     Cost(150 gold)" << endl;
+                cout << "3) Admin's Armor:      Defense(20)     Durability(400)     Cost(500 gold)" << endl;
+                cout << "4) Phantom's Armor:    Defense(100)    Durability(1000)    Cost(1000 gold)" << endl;
+                cout << "5) Memory Loss:        Defense(400)    Durability(4000)    Cost(250000 gold)" << endl;
+                cout << "6) Frozen Hell:        Defense(800)    Durability(8000)    Cost(750000 gold)" << endl;
+                cout << "7) Hero's Redemption:  Defense(1200)   Durability(10000)   Cost(1000000 gold)" << endl;
+                cout << "or type in -1 to return to the menu: ";
+                cin >> a;
+                while (a == 0 || a < -1 || a > 7)
+                {
+                    cout << "Invalid option." << endl;
+                    cout << "Type in options 1-7 or -1: ";
+                    cin >> a;
+                }
+                if (a == 1)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Successfully purchased 1 Beginner Armor, 50 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Beginner Armor");
+                    // Deduct gold from the account
+                    break;
+                }
+                if (a == 2)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Successfully purchased 1 Ebon Armor, 150 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Ebon Armor");
+                    // Deduct gold from the account
+                    break;
+                }
+                if (a == 3)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Successfully purchased 1 Admin's Armor, 500 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Admin's Armor");
+                    // Deduct gold from the account
+                    break;
+                }
+                if (a == 4)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Successfully purchased 1 Phantom's Armor, 1000 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Phantom's Armor");
+                    // Deduct gold from the account
+                    break;
+                }
+                if (a == 5)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Successfully purchased 1 Memory Loss, 250000 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Memory Loss");
+                    // Deduct gold from the account
+                    break;
+                }
+                if (a == 6)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Successfully purchased 1 Frozen Hell, 750000 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Frozen Hell");
+                    // Deduct gold from the account
+                    break;
+                }
+                if (a == 7)
+                {
+                    // Say something here about not having enough gold
+                    cout << "Congrats on purchasing the most god-like armor in the game! 1000000 gold will now be taken out of your account." << endl;
+                    inventory.push_back("Hero's Redemption");
+                    // Deduct gold from the account
+                    break;
+                }
             case 4:
-                string potion;
                 cout << "Type in 1 to sell a potion, 2 to sell an item, or 3 to return to the menu: ";
                 cin >> option4;
                 while (option4 <= 0 || option4 > 3)
@@ -279,15 +371,170 @@ void store(vector<string>& inventory, vector<string>& potions)
                     }
                     if (ans == 1)
                     {
-                        string name;
-                        cout << "Type in the first part of the name of the weapon you would like to sell "
-                        << "(note: with weapons with just one name, type just that one name): ";
-                        cin >> name;
-                        
+                        int ans, count = 0;
+                        string name, name1, name2;
+                        cout << "Type in 1 for a one-named sword, or 2 for a two-named sword: ";
+                        cin >> ans;
+                        while (ans <= 0 || ans > 2)
+                        {
+                            cout << "Not a valid option." << endl;
+                            cout << "Type in 1 or 2: ";
+                            cin >> ans;
+                        }
+                        if (ans == 1)
+                        {
+                            cout << "Enter the name: ";
+                            cin >> name;
+                        }
+                        if (ans == 2)
+                        {
+                            cout << "Enter the name: ";
+                            cin >> name1 >> name2;
+                            name = name1 + " " + name2;
+                        }
+                        for (unsigned int i = 0; i < inventory.size(); i++)
+                        {
+                            if (!(name == inventory[i]))
+                            {
+                                count++;
+                            }
+                            if (name == inventory[i])
+                            {
+                                if (inventory[i] == "Beginner Sword")
+                                {
+                                    cout << "One Beginner Sword has been sold for 25 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Claymore")
+                                {
+                                    cout << "One Claymore has been sold for 75 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Gladius")
+                                {
+                                    cout << "One Gladius has been sold for 250 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Soul Eater")
+                                {
+                                    cout << "One Soul Eater has been sold for 500 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Flame Dance")
+                                {
+                                    cout << "One Flame Dance has been sold for 5000 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Heartless Crusader")
+                                {
+                                    cout << "One Heartless Crusader has been sold for 50000 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Eternal Harmony")
+                                {
+                                    cout << "One Eternal Harmony has been sold for 250000 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Elucidator")
+                                {
+                                    cout << "One Elucidator has been sold for 500000 gold." << endl;
+                                    // Add the gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                            }
+                        }
+                        if (count == inventory.size())
+                        {
+                            cout << "There is no such weapon in your inventory." << endl;
+                            cout << "Returning to the menu." << endl;
+                        }
                     }
                     if (ans == 2)
                     {
-                        
+                        int count = 0;
+                        string armor1, armor2, armor;
+                        cout << "Type in the name of the armor you wish to sell: ";
+                        cin >> armor1 >> armor2;
+                        armor = armor1 + " " + armor2;
+                        for (unsigned int i = 0; i < inventory.size(); i++)
+                        {
+                            if (!(armor == inventory[i]))
+                            {
+                                count++;
+                            }
+                            if (armor == inventory[i])
+                            {
+                                if (inventory[i] == "Beginner Armor")
+                                {
+                                    cout << "One Beginner Armor has been sold for 25 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Ebon Armor")
+                                {
+                                    cout << "One Ebon Armor has been sold for 75 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Admin's Armor")
+                                {
+                                    cout << "One Admin's Armor has been sold for 250 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Phantom's Armor")
+                                {
+                                    cout << "One Phantom's Armor has been sold for 500 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Memory Loss")
+                                {
+                                    cout << "One Memory Loss has been sold for 125000 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Frozen Hell")
+                                {
+                                    cout << "One Frozen Hell has been sold for 375000 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                                if (inventory[i] == "Hero's Redemption")
+                                {
+                                    cout << "One Hero's Redemption has been sold for 500000 gold." << endl;
+                                    // Add gold into account.
+                                    inventory[i] = " ";
+                                    break;
+                                }
+                            }
+                        }
+                        if (count == inventory.size())
+                        {
+                            cout << "No such armor exists." << endl;
+                            cout << "Returning to the menu." << endl;
+                        }
                     }
                 }
         }
