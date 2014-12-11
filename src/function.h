e36552e3e3f2a013d25fc57d284e5a2395a0b9c9
Andrew Namgoong <AGoon105@users.noreply.github.com>
2014-12-11 13:23:03 -0800

    Included the wait time

--- src/function.h
+++ src/function.h
@@ -1,6 +1,7 @@
 #ifndef __FUNCTION_H_INCLUDED__
 #define __FUNCTION_H_INCLUDED__
 
+#include <ctime>
 class Player;
 class Monster;
 
@@ -11,5 +12,7 @@ int battle_menu();
 void turn(Player& Tim, Monster creature);
 
 void display_battle(int Tim_health, int enemy_health);
+
+void wait_time(clock_t sec);
 
 #endif
