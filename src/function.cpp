647e6c3565967cb07a78d9a3392f40df0532d45b
Andrew Namgoong <AGoon105@users.noreply.github.com>
2014-12-11 12:59:44 -0800

    Included random number generation display for flinching

--- src/function.cpp
+++ src/function.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "function.h"
 #include "Monster.h"
 #include "Player.h"
@@ -7,23 +9,51 @@ using namespace std;
 
 void turn(Player& Tim, Monster creature)
 {
+    srand(time(0));
     int choice, Tim_damage, creature_damage;
     bool flee = false;
     cout << "Tim sees a creature! Tim has poor vision, so the identity of the creature is unknown." << endl;
     do
     {
+        int r = rand(), chance1 = (r % 10) + 1, chance2 = (r % 10) + 1;
         display_battle(Tim.get_health(), creature.get_health());
         choice = battle_menu();
         if(choice == 1)
         {
             Tim_damage = Tim.get_damage();
             creature_damage = creature.get_damage();
-            creature.hurt(Tim_damage);
-            cout << "The creature was hurt for " << Tim_damage << " damage!" << endl;
+            if (chance1 == 1 || chance1 == 2)
+            {
+                creature.hurt((Tim_damage * 3) / 2);
+                cout << "Tim hits the creature with a critical hit! The creature was hurt for " << (Tim_damage * 3) / 2 << " damage!" << endl;
+            }
+            else if (chance1 == 9 || chance1 == 10)
+            {
+                creature.hurt(Tim_damage / 2);
+                cout << "Tim fumbles with the weapon and the creature seems undazed. The creature was hurt for " << Tim_damage / 2 << " damage!" << endl;
+            }
+            else if (chance1 != 1 && chance1 != 2 && chance1 != 9 && chance1 != 10)
+            {
+                creature.hurt(Tim_damage);
+                cout << "The creature was hurt for " << Tim_damage << " damage!" << endl;
+            }
             if(creature.check_life())
             {
-                Tim.hurt(creature_damage);
-                cout << "Tim was hurt for " << creature_damage << " damage!" << endl;
+                if (chance2 == 1)
+                {
+                    Tim.hurt(creature_damage * 2);
+                    cout << "The creature lands a critical hit! Tim was hurt for " << creature_damage * 2 << " damage!" << endl;
+                }
+                else if (chance2 == 2 || chance2 == 3)
+                {
+                    Tim.hurt(creature_damage / 2);
+                    cout << "The creature trips and falls on its face! Tim was hurt for " << creature_damage / 2 << " damage!" << endl;
+                }
+                else if (chance2 != 1 && chance2 != 2 && chance2 != 3)
+                {
+                    Tim.hurt(creature_damage);
+                    cout << "Tim was hurt for " << creature_damage << " damage!" << endl;
+                }
             }
             else
             {
@@ -74,21 +104,63 @@ int menu()
 
 void display_battle(int Tim_health, int enemy_health)
 {
-    cout << endl;
-    cout << "         Timothy                       Beginner's Slime    " << endl;
-    cout << "         HP: " << Tim_health << "                            HP: " << enemy_health << "          " << endl;
-    cout << "           ---                                             " << endl;
-    cout << "          [. .]                                     ____   " << endl;
-    cout << "          [   ]  |                            ______|  |   " << endl;
-    cout << "     ----  ---   |                            |     |__|   " << endl;
-    cout << "     |  |   |    |                            |            " << endl;
-    cout << "     |  |---|----|                     _______|_______     " << endl;
-    cout << "     ----   |                          |             |     " << endl;
-    cout << "            |                          |  []     []  |     " << endl;
-    cout << "         -------                       |             |     " << endl;
-    cout << "         |     |                       |  |_______|  |     " << endl;
-    cout << "         |     |                       |_____________|     " << endl;
-    cout << "-----------------------------------------------------------" << endl;
+    Player Tim;
+    Monster creature;
+    const int max_health = creature.get_health();
+    if (enemy_health == max_health)
+    {
+        cout << endl;
+        cout << "         Timothy                       Beginner's Slime    " << endl;
+        cout << "         HP: " << Tim_health << "                            HP: " << enemy_health << "          " << endl;
+        cout << "           ---                                             " << endl;
+        cout << "          [. .]                                     ____   " << endl;
+        cout << "          [   ]  |                            ______|  |   " << endl;
+        cout << "     ----  ---   |                            |     |__|   " << endl;
+        cout << "     |  |   |    |                            |            " << endl;
+        cout << "     |  |---|----|                     _______|_______     " << endl;
+        cout << "     ----   |                          |             |     " << endl;
+        cout << "            |                          |  []     []  |     " << endl;
+        cout << "         -------                       |             |     " << endl;
+        cout << "         |     |                       |  |_______|  |     " << endl;
+        cout << "         |     |                       |_____________|     " << endl;
+        cout << "-----------------------------------------------------------" << endl;
+    }
+    if (enemy_health < max_health)
+    {
+        cout << endl;
+        cout << "         Timothy                       Beginner's Slime    " << endl;
+        cout << "         HP: " << Tim_health << "                            HP: " << enemy_health << "          " << endl;
+        cout << "           ---                                             " << endl;
+        cout << "          [. .]                                     ____   " << endl;
+        cout << "          [   ]  |                            ______|  |   " << endl;
+        cout << "     ----  ---   |                            |     |__|   " << endl;
+        cout << "     |  |   |    |                            |            " << endl;
+        cout << "     |  |---|----|                         ___|_______     " << endl;
+        cout << "     ----   |                             _|     XX  |     " << endl;
+        cout << "            |                           _| XX        |     " << endl;
+        cout << "         -------                       |             |     " << endl;
+        cout << "         |     |                       |  |_______|  |     " << endl;
+        cout << "         |     |                       |_____________|     " << endl;
+        cout << "-----------------------------------------------------------" << endl;
+
+        wait_time(1000);
+    
+        cout << endl;
+        cout << "         Timothy                       Beginner's Slime    " << endl;
+        cout << "         HP: " << Tim_health << "                            HP: " << enemy_health << "          " << endl;
+        cout << "           ---                                             " << endl;
+        cout << "          [. .]                                     ____   " << endl;
+        cout << "          [   ]  |                            ______|  |   " << endl;
+        cout << "     ----  ---   |                            |     |__|   " << endl;
+        cout << "     |  |   |    |                            |            " << endl;
+        cout << "     |  |---|----|                     _______|_______     " << endl;
+        cout << "     ----   |                          |             |     " << endl;
+        cout << "            |                          |  []     []  |     " << endl;
+        cout << "         -------                       |             |     " << endl;
+        cout << "         |     |                       |  |_______|  |     " << endl;
+        cout << "         |     |                       |_____________|     " << endl;
+        cout << "-----------------------------------------------------------" << endl;
+    }
 }
 
 int battle_menu()
@@ -107,4 +179,11 @@ int battle_menu()
     } while(response < 1 or response > 2);
 
     return response;
+}
+
+void wait_time(clock_t sec)
+{
+    clock_t start_time = clock();
+	clock_t end_time = start_time + sec * 1000;
+	while(clock() < end_time);
 }
