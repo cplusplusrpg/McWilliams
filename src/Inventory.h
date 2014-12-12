f942e60260004a178e2ff415db5a3fdf974de72c
Matthew Oberlies <moberlies1337@gmail.com>
2014-12-11 19:48:09 -0800

    Document Inventory.h
    Added much needed documentation.

--- src/Inventory.h
+++ src/Inventory.h
@@ -1,3 +1,7 @@
+//The Inventory class. A Player will have an Inventory to store any and all Items he has.
+//At the moment, the only Items that exist are Weapons, Armors, and Potions, but as more are created, the Inventory class will be given more vectors.
+//All member functions are explained in the implementation file: Inventory.cpp
+
 #ifndef __INVENTORY_H_INCLUDED__
 #define __INVENTORY_H_INCLUDED__
 
@@ -17,5 +21,8 @@ private:
     std::vector<Armor> armors;
     std::vector<Potion> potions;
 };
+//The Inventory class contains a vector that contains a subset of Items. 1 vector for each type.
+//1 vector that contains Items was considered, but such an implementation would lead to an overall lack of organization.
+//So, multiple vectors of different derived Items are used instead.
 
 #endif
