4d49a2bd4aee274a9ccbe4c249eb6ff3ee95ebd9
Matthew Oberlies <moberlies1337@gmail.com>
2014-12-11 19:55:40 -0800

    Document rpg.cpp
    Added much needed documentation.

--- src/rpg.cpp
+++ src/rpg.cpp
@@ -1,3 +1,5 @@
+//The core program. This file will pull in all of the other header files and is the main organizer of the Timothy McWilliams project.
+
 #include <iostream>
 #include <vector>
 #include "Monster.h"
@@ -8,21 +10,22 @@
 
 using namespace std;
 
+//The program still needs a lot of work and is quite basic for the time being.
 int main()
 {
     Player Tim;
-    Monster Slime;
+    Monster Slime;//1 Player and 1 Monster are created.
     int choice;
 
     cout << "These are the Adventures of Timothy McWilliams. The greatest story ever told." << endl;
     cout << "The fate of Timothy McWilliams depends on you." << endl << endl;
     do
     {
-        choice = menu();
+        choice = menu();//After asking the user what they would like to do,
         
         if(choice == 1)
         {
-            turn(Tim, Slime);
+            turn(Tim, Slime);//Performs an appropriate action.
         }
         else if(choice == 2)
         {
