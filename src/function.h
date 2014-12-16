2fe1d859dd12ac6a17a6930d151728e7562b4ae9
Andrew Namgoong <AGoon105@users.noreply.github.com>
2014-12-16 00:56:00 -0800

    Updated

--- src/function.h
+++ src/function.h
@@ -1,5 +1,3 @@
-//These are not the droids you are looking for...Move along. Move along.
-
 #ifndef __FUNCTION_H_INCLUDED__
 #define __FUNCTION_H_INCLUDED__
 
@@ -11,9 +9,5 @@ int menu();
 int battle_menu();
 
 void turn(Player& Tim, Monster creature);
-
-void display_battle(int Tim_health, int enemy_health);
-
-//These are just the function declarations for the functions explained in function.cpp
 
 #endif
